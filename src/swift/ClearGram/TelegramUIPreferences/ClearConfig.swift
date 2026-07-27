d19a38fb51cb53a757a304f0cf676501615d355f
asyasocute <asya_bububu@proton.me>
2026-07-28 01:21:32 +0700

    feat: flatStickerCorners config

--- src/swift/ClearGram/TelegramUIPreferences/ClearConfig.swift
+++ src/swift/ClearGram/TelegramUIPreferences/ClearConfig.swift
@@ -31,6 +31,49 @@ public enum ClearConfig {
     // individual toggles — add as ClearConfigSettings grows
     public static var hideStories: Bool { current().hideStories }
     public static var hideAiFeatures: Bool { current().hideAiFeatures }
+    public static var secondsInMessages: Bool { current().secondsInMessages }
+    public static var confirmCalls: Bool { current().confirmCalls }
+    public static var doubleTapToEdit: Bool { current().doubleTapToEdit }
+    public static var showProfileId: Bool { current().showProfileId }
+    public static var showDC: Bool { current().showDC }
+    public static var hidePhoneInSettings: Bool { current().hidePhoneInSettings }
+    public static var hideContextMenuReply: Bool { current().hideContextMenuReply }
+    public static var hideContextMenuPin: Bool { current().hideContextMenuPin }
+    public static var hideContextMenuForward: Bool { current().hideContextMenuForward }
+    public static var hideContextMenuReport: Bool { current().hideContextMenuReport }
+    public static var hideContextMenuSelect: Bool { current().hideContextMenuSelect }
     public static var doubleTapDelay: Int32 { current().doubleTapDelay }
     public static var animationSpeed: Double { current().animationSpeed }
+    public static var defaultEmojisFirst: Bool { current().defaultEmojisFirst }
+    public static var disableScrollToNextChannel: Bool { current().disableScrollToNextChannel }
+    public static var showInlineReactions: Bool { current().showInlineReactions }
+    public static var blockCloudDrafts: Bool { current().blockCloudDrafts }
+    public static var showForwardedTime: Bool { current().showForwardedTime }
+    public static var noSelectionCap: Bool { current().noSelectionCap }
+    public static var stripTrackingParams: Bool { current().stripTrackingParams }
+    public static var replacePreviewLinks: Bool { current().replacePreviewLinks }
+    public static var confirmInternalLinks: Bool { current().confirmInternalLinks }
+    public static var biometricConfirmation: Bool { current().biometricConfirmation }
+    public static var hideSponsoredMessages: Bool { current().hideSponsoredMessages }
+    public static var hideStarReactionButton: Bool { current().hideStarReactionButton }
+    public static var hideStarReactionCount: Bool { current().hideStarReactionCount }
+    public static var hideSimilarChannels: Bool { current().hideSimilarChannels }
+    public static var warnPollsRevote: Bool { current().warnPollsRevote }
+    public static var showPackOwner: Bool { current().showPackOwner }
+    public static var timeOnServiceMessages: Bool { current().timeOnServiceMessages }
+    public static var showTabNames: Bool { current().showTabNames }
+    public static var compactChatList: Bool { current().compactChatList }
+    public static var chatListLines: Int32 { current().chatListLines }
+    public static var compactFolderNames: Bool { current().compactFolderNames }
+    public static var allChatsHidden: Bool { current().allChatsHidden }
+    public static var hideTabBar: Bool { current().hideTabBar }
+    public static var wideTabBar: Bool { current().wideTabBar }
+    public static var tabBarSearchEnabled: Bool { current().tabBarSearchEnabled }
+    public static var wideChannelPosts: Bool { current().wideChannelPosts }
+    public static var hideChannelBottomButton: Bool { current().hideChannelBottomButton }
+    public static var disableGalleryCamera: Bool { current().disableGalleryCamera }
+    public static var disableGalleryCameraPreview: Bool { current().disableGalleryCameraPreview }
+    public static var disableStoryCameraSwipe: Bool { current().disableStoryCameraSwipe }
+    public static var enableMultiColumnLayout: Bool { current().enableMultiColumnLayout }
+    public static var flatStickerCorners: Bool { current().flatStickerCorners }
 }
